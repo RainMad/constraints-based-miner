1ca88e4f1e00b2a7cc7e130e789a98a987f9eca6
Rainer Madlmayr <s1710595011@students.fh-hagenberg.at>
2019-03-05 14:18:21 +0100

    modified the test

--- tests/testthat/test-constraints.R
+++ tests/testthat/test-constraints.R
@@ -8,7 +8,7 @@ test_that("Application works", {
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
-  expect_pass(testApp("../../R", compareImages = FALSE))
+  expect_pass(testApp("R", compareImages = FALSE))
 })
 
 test_that("Constratint works", {
