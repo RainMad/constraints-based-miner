b2c803e8c28d58b22dbf36738b30aa18b8edb183
Rainer Madlmayr <s1710595011@students.fh-hagenberg.at>
2019-05-22 08:25:01 +0200

    - Changed the upload limit to 200MB - Moved the reactive values to the server part - Renaming of variables - Additional check if a constraint with the same activities is already added - Additional check if filtered events are available for export -

--- R/app.R
+++ R/app.R
@@ -6,7 +6,7 @@ library(processanimateR)
 library(xesreadR)
 library(tidyverse)
 
-options(shiny.maxRequestSize = 1 * 1024 ^ 3) # upload limit = 1GB
+options(shiny.maxRequestSize = 200 * 1024 ^ 2) # upload limit = 200MB (GB 1 * 1024^3)
 
 # All constraints return an eventlog as result which looks like the following
 # <Trace1> <Boolean>
@@ -356,50 +356,51 @@ ui <- fluidPage(
   
 )
 
-filtered_events <- NaN
-
-# Example data
-RV <-
-  reactiveValues(
-    constraints = data.frame(
-      id = NULL,
-      x = NULL,
-      y = NULL,
-      xy = NULL,
-      Delete = NULL,
-      stringsAsFactors = FALSE
-    ),
-    rowname = NULL,
-    eventlog = NULL,
-    filters = NULL
-  )
 
 server <- function(input, output, session) {
   # counter value used as id for the releationships
   counter <- reactiveValues(countervalue = 0)
+
+  filtered_events <- NaN
+  
+  # Example data
+  reactiveDataValues <-
+    reactiveValues(
+      constraints = data.frame(
+        Id = NULL,
+        ActivityA = NULL,
+        ActivityB = NULL,
+        Constraint = NULL,
+        Filtered = NULL,
+        Delete = NULL
+      ),
+      eventlog = NULL,
+      filters = NULL
+    )
   
   # remove UI for second activity if single constraint is selected
   output$activity2_input <- renderUI({
     if (input$z %in% dual_constraints) {
-      if (is.null(RV$eventlog)) {
+      if (is.null(reactiveDataValues$eventlog)) {
         possible_activities <- "None"
       } else {
-        possible_activities <- unique(RV$eventlog$activity_id)
+        possible_activities <- unique(reactiveDataValues$eventlog$activity_id)
       }
       
       fluidRow(selectInput("y", possible_activities, label = "Activity B"))
     }
   })
+
   
   # read file only if fileinput changes
   observeEvent(input$xes_input, {
-    RV$eventlog <- read_xes(input$xes_input$datapath)
-    RV$filters <-
-      data.frame(row.names = unique(RV$eventlog$CASE_concept_name))
-    RV$constraints <- NULL
+    reactiveDataValues$eventlog <- read_xes(input$xes_input$datapath)
+    reactiveDataValues$filters <-
+      data.frame(row.names = unique(reactiveDataValues$eventlog$CASE_concept_name))
+    reactiveDataValues$constraints <- NULL
     
     # set content of activity dropdown boxes
-    possible_activities <- unique(RV$eventlog$activity_id)
+    possible_activities <- unique(reactiveDataValues$eventlog$activity_id)
     
     updateSelectInput(session = session,
                       inputId = "x",
@@ -411,14 +412,15 @@ server <- function(input, output, session) {
   
   # render graph
   output$process <- renderProcessanimater(expr = {
-    if (is.null(RV$eventlog)) {
+
+    if (is.null(reactiveDataValues$eventlog)) {
       return()
     }
     
-    if (length(RV$filters) == 0) {
+    if (length(reactiveDataValues$filters) == 0) {
       event_filter <- data.frame(case_id = c())
     } else {
-      event_filter <- data.frame(RV$filters)
+      event_filter <- data.frame(reactiveDataValues$filters)
       event_filter$alltrue <-
         rowSums(event_filter) == length(event_filter)
       event_filter <- event_filter %>%
@@ -426,8 +428,8 @@ server <- function(input, output, session) {
         filter(!alltrue)
     }
     
-    filtered_events <<-
-      RV$eventlog %>% filter(!CASE_concept_name %in% event_filter$case_id)
+    filtered_events <-
+      reactiveDataValues$eventlog %>% filter(!CASE_concept_name %in% event_filter$case_id)
     
     if (count(filtered_events) == 0) {
       return()
@@ -448,6 +450,24 @@ server <- function(input, output, session) {
   
   # action which is fired when pressing the Ok Button for inserting constraints
   observeEvent(input$act, {
+    
+    if(is.null(reactiveDataValues$eventlog))
+      return()
+    
+    constraints <- reactiveDataValues$constraints
+    # check if the constraint with the same activities already exists 
+    if(input$z %in% constraints$Constraint && 
+       input$x %in% constraints$ActivityA &&
+       input$y %in% constraints$ActivityB)
+    {
+      showModal(modalDialog(
+        title = "Constraint already added",
+        paste0("The constraint for the selected activities has already been added."),
+        easyClose = TRUE,
+        footer = NULL
+      ))
+      return()
+    }
     # constraints
     constraint = switch(
       input$z,
@@ -473,19 +493,19 @@ server <- function(input, output, session) {
     
     if (input$z %in% dual_constraints) {
       tbl_activity_b <- input$y
-      constraint_matches = constraint(RV$eventlog, input$x, input$y)
+      constraint_matches = constraint(reactiveDataValues$eventlog, input$x, input$y)
     } else {
       tbl_activity_b <- "-"
-      constraint_matches = constraint(RV$eventlog, input$x)
+      constraint_matches = constraint(reactiveDataValues$eventlog, input$x)
     }
     
-    # dirty hacks to merge new constraints with old constraints
+    # merge new constraints with old constraints
     constraint_matches <- constraint_matches %>%
       column_to_rownames("CASE_concept_name")
     colnames(constraint_matches) <- toString(counter$countervalue)
-    RV$filters = merge(RV$filters, constraint_matches, by = "row.names") %>%
-      remove_rownames %>%
-      column_to_rownames(var = "Row.names")
+    
+    reactiveDataValues$filters = merge(reactiveDataValues$filters, constraint_matches, by = "row.names") %>%
+       column_to_rownames(var = "Row.names")
     
     # create a new entry
     newrow = data.frame(
@@ -508,11 +528,11 @@ server <- function(input, output, session) {
     # increase the id
     counter$countervalue = counter$countervalue + 1
     # add the entry to the table
-    RV$constraints <<- rbind(RV$constraints, newrow)
+    reactiveDataValues$constraints <- rbind(reactiveDataValues$constraints, newrow)
   })
   
   observeEvent(input$export, {
-    if(nrow(filtered_events) == 0){
+    if(nrow(filtered_events) == 0 || is.nan(filtered_events)){
       showModal(modalDialog(
         title = "Error",
         paste0("Process model does not contain data"),
@@ -544,7 +564,7 @@ server <- function(input, output, session) {
   
   # print the table
   output$table <- DT::renderDataTable(
-    RV$constraints,
+    reactiveDataValues$constraints,
     escape = FALSE,
     rownames = FALSE,
     options = list(
@@ -562,11 +582,11 @@ server <- function(input, output, session) {
     # get the id of the button which is the same like in the data frame
     selectedId <-
       as.numeric(strsplit(input$select_button, "_")[[1]][2])
-    RV$filters[[toString(selectedId)]] <- NULL
+    reactiveDataValues$filters[[toString(selectedId)]] <- NULL
     
     # Remove the value of the table
-    RV$constraints <<-
-      RV$constraints[-which(RV$constraints$Id == selectedId),]
+    reactiveDataValues$constraints <-
+      reactiveDataValues$constraints[-which(reactiveDataValues$constraints$Id == selectedId),]
   })
   
   observeEvent(input$z, {
@@ -595,7 +615,7 @@ server <- function(input, output, session) {
     })
   })
   
-  exportTestValues(RV = { RV$eventlog })
+  # exportTestValues(RV = { RV$eventlog })
 }
 
 # Run the application
